6217eeae83fbe600e99c0a448c4b4c2431b0f51a
Lucy0112358 <lusinehsahakian@gmail.com>
2023-02-24 19:40:16 +0400

    add: added Login component

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,19 +1,15 @@
 import React from "react";
-import "../styles/Login.css"
+import "../styles/Login.css";
 
 function Login() {
   return (
     <>
       <form noValidate>
         <label>Enter your username </label>
-
-        <input
-          type="email"
-          id="email"
-          pattern=".+@globex\.com"
-          size="30"
-          required
-        />
+        <input type="text" id="username" size="30" required />
+        <label>Enter your password </label>
+        <input type="password" id="pass" size="16" required />
+        <button type="submit">Login</button>
       </form>
     </>
   );
